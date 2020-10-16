b1b8a66b6d1fdb80700aee06ad52b5decea7900e
Laurino108 <72888948+Laurino108@users.noreply.github.com>
2020-10-16 12:52:25 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,16 @@
 <?php
-define('DBSERVER'
-define
-define
-define
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'gamekid@localhost');
+define('DB_USERNAME', 'gamekid');
+define('DB_PASSWORD', 'l1080510');
+define('DB_NAME', 'gamekid');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
