8cf1d4ffeff3a4b440fe2feb6f4206b530aabc16
Laurino108 <72888948+Laurino108@users.noreply.github.com>
2020-10-16 13:00:33 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,40 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'gamekid');
-define('DB_PASSWORD', 'l1080510');
-define('DB_NAME', 'gamekid');
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($username)){
+if (!empty($password)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "gamekid";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO account (username, password)
+values ('$username','$password')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
 }
 ?>
